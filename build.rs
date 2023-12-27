afc607e74a72678aa4f99a4ff9c3db8a5432f753
Marcin Serwin <marcin.serwin0@protonmail.com>
2023-12-27 19:55:38 +0100

    Fix macOS building

--- build.rs
+++ build.rs
@@ -7,25 +7,24 @@ fn get_ext(filename: &str) -> &str {
 }
 
 fn main() {
-    #[cfg(target_os = "macos")]
-    println!(
-        r"cargo:rustc-link-search={}",
-        env::var("DYLD_FALLBACK_LIBRARY_PATH").unwrap()
-    );
+    if let Ok(path) = env::var("VULKAN_SDK_LIB_PATH") {
+        println!(r"cargo:rustc-link-search={path}");
+    }
 
     let out_dir = env::var("OUT_DIR").unwrap();
+    let glslc_bin = env::var("GLSLC_BIN").unwrap_or("glslc".to_string());
     const SHADERS_DIR: &'static str = "src/shaders";
     const SHADERS: [&'static str; 2] = ["shader.vert", "shader.frag"];
     SHADERS.into_iter().for_each(|shader| {
         assert!(
-            Command::new("glslc")
+            Command::new(glslc_bin.as_str())
                 .args(&[
                     format!("{}/{}", SHADERS_DIR, shader).as_str(),
                     "-o",
                     format!("{}/{}.spv", out_dir, get_ext(shader)).as_str(),
                 ])
                 .status()
-                .unwrap()
+                .expect("failed to execute shader compilation command")
                 .success(),
             "compilation of '{shader}' failed"
         );
