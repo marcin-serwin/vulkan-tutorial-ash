a16bdb2a84986f828581b764f8f26605c38ae875
Marcin Serwin <marcin.serwin0@protonmail.com>
2024-01-20 09:06:28 +0100

    Use polling event loop

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,23 @@
 use ash::Entry;
-use std::sync::{atomic::AtomicU8, atomic::Ordering};
 use vulkan_tutorial_ash::HelloTriangleApplication;
 use winit::event::*;
 use winit::keyboard::*;
 use winit::{event_loop::EventLoop, window::Window};
 
 fn main() {
-    static FPS: AtomicU8 = AtomicU8::new(60);
     let event_loop = EventLoop::new().unwrap();
-    let window = std::sync::Arc::new(Window::new(&event_loop).unwrap());
+    let window = Window::new(&event_loop).unwrap();
     let entry = Entry::linked();
 
     let mut app = HelloTriangleApplication::new(&entry, &window);
+    let mut fps: u8 = 60;
 
     event_loop.set_control_flow(winit::event_loop::ControlFlow::Wait);
-
-    let win = std::sync::Arc::downgrade(&window);
-
-    std::thread::spawn(move || {
-        while let Some(window) = win.upgrade() {
-            window.request_redraw();
-            std::thread::sleep(std::time::Duration::from_secs_f64(
-                1.0 / (FPS.load(Ordering::Relaxed) as f64),
-            ));
-        }
-    });
 
     window
         .set_cursor_grab(winit::window::CursorGrabMode::Locked)
         .unwrap();
+    event_loop.set_control_flow(winit::event_loop::ControlFlow::Poll);
     event_loop
         .run(|event, elwt| match event {
             Event::WindowEvent {
@@ -75,20 +64,12 @@ fn main() {
 
                 if event.state == ElementState::Pressed && !event.repeat {
                     use KeyCode::*;
-                    match code {
-                        Digit1 => {
-                            FPS.store(10, Ordering::Relaxed);
-                        }
-                        Digit2 => {
-                            FPS.store(30, Ordering::Relaxed);
-                        }
-                        Digit3 => {
-                            FPS.store(60, Ordering::Relaxed);
-                        }
-                        Digit4 => {
-                            FPS.store(144, Ordering::Relaxed);
-                        }
-                        _ => {}
+                    fps = match code {
+                        Digit1 => 10,
+                        Digit2 => 30,
+                        Digit3 => 60,
+                        Digit4 => 144,
+                        _ => fps,
                     }
                 }
             }
@@ -97,6 +78,10 @@ fn main() {
                 ..
             } => {
                 app.handle_mouse_movement(delta);
+            }
+            Event::AboutToWait => {
+                std::thread::sleep(std::time::Duration::from_secs_f64(1.0 / (fps as f64)));
+                window.request_redraw();
             }
             _ => {
                 // println!("{event:?}");
